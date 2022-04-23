8158268fe832ead1f9fbbffad0795ac9e9e3d9a6
Kojababuxd <104275480+Kojababuxd@users.noreply.github.com>
2022-04-24 00:51:17 +0500

    Update Xd.py

--- Xd.py
+++ Xd.py
@@ -34,14 +34,6 @@ bd = random.randint(2e7, 3e7)
 sim = random.randint(2e4, 4e4)
 header = {'x-fb-connection-bandwidth': repr(bd), 'x-fb-sim-hni': repr(sim), 'x-fb-net-hni': repr(sim),'x-fb-connection-quality': 'EXCELLENT', 'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.3','x-fb-connection-type': 'unknown','content-type': 'application/x-www-form-urlencoded', 'x-fb-http-engine': 'Liger'}
 
-
-
-                                _  __ ____     _     _ 
-/ |/ //  _ \   / |/\ / |
-|   / | / \|   | |\_\| |
-|   \ | \_/|/\_| |   | |
-\_|\_\\____/\____/   \_|
-
                         
                               
 \n\033[0m----------------------------------
