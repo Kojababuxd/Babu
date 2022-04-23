f77222d88697cfb77ee6c3e05fcf81e750e19eb7
Kojababuxd <104275480+Kojababuxd@users.noreply.github.com>
2022-04-24 00:56:07 +0500

    Update Xd.py

--- Xd.py
+++ Xd.py
@@ -34,8 +34,18 @@ bd = random.randint(2e7, 3e7)
 sim = random.randint(2e4, 4e4)
 header = {'x-fb-connection-bandwidth': repr(bd), 'x-fb-sim-hni': repr(sim), 'x-fb-net-hni': repr(sim),'x-fb-connection-quality': 'EXCELLENT', 'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.3','x-fb-connection-type': 'unknown','content-type': 'application/x-www-form-urlencoded', 'x-fb-http-engine': 'Liger'}
 
-                        
-                              
+
+
+logo = """
+
+                                
+\033[1;91m█   ██  ██████       ██  █████  
+██  ██  ██    ██      ██ ██   ██ 
+█████   ██    ██      ██ ███████ 
+██  ██  ██    ██ ██   ██ ██   ██ 
+██   ██  ██████   █████  ██   █
+
+                       
 \n\033[0m----------------------------------
 \033[1;92mAuthor \033[1;93m: Muhammad Ali
 \033[1;92mFb \033[1;93m: Koja Babu Here
@@ -160,7 +170,8 @@ def token():
         sav.close()
         login()
 
-#------------------------------------
+#-----------------------------
+
 
 
 def menu():
